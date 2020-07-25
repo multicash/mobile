ea6b452542993a3f7110e2bf6d5d6e4547c0a294
marpme <marpme@users.noreply.github.com>
2020-07-25 09:45:09 +0200

    Remove usless test stuff

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -2,13 +2,6 @@
  * @format
  */
 
-// import 'react-native';
-import React from 'react';
-import App from '../App';
-
-// Note: test renderer must be required after react-native.
-// import renderer from 'react-test-renderer';
-
 it('renders correctly', () => {
   expect(true).toBe(true)
 });
