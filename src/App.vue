adf5f7ad4a79741594f58b586b1bef3396eed6f8
marpme <marpme@users.noreply.github.com>
2020-07-27 13:05:29 +0200

    Implement app navigation possibility

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
-  <home-view/>
+  <app-navigation/>
 </template>
 
 <script>
+import {
+  createAppContainer,
+  createStackNavigator,
+} from "vue-native-router";
+
 import HomeView from './views/HomeView'
+import PayView from './views/PayView'
+
+const StackNavigator = createStackNavigator(
+  {
+    Home: HomeView,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const AppNavigation = createAppContainer(StackNavigator);
 
 export default {
-	name: 'App',
-  components: {HomeView}
+  name: 'App',
+  components: { AppNavigation },
 }
 </script>
