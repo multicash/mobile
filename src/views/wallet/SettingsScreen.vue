d9f2296e5d1fdb3bec59858c6ffeae28294e3dc1
Swen van Zanten <info@hdsign.nl>
2020-09-13 23:53:57 +0200

    add wallet settings screen

--- src/views/wallet/SettingsScreen.vue
+++ src/views/wallet/SettingsScreen.vue
@@ -2,24 +2,70 @@
   <view :style="{ flex: 1 }">
     <status-bar bar-style="light-content" />
     <modal-navigation
+      title="Settings"
       has-back-button
       @on-dismiss="navigation.goBack()"
     >
     </modal-navigation>
-    <view-background>
-      <text>Settings</text>
-    </view-background>
+    <table-view :sections="data"/>
   </view>
 </template>
 
 <script>
 import ModalNavigation from '@/components/ModalNavigation'
-import ViewBackground from '@/components/ViewBackground'
+import TableView from '@/components/TableView'
 
 export default {
   name: 'SettingsScreen',
 
-  components: { ViewBackground, ModalNavigation },
+  components: { TableView, ModalNavigation },
+
+  data () {
+    return {
+      data: [
+        {
+          title: 'Preferences',
+          data: [
+            {
+              title: 'Name',
+              subtitle: 'The wallets name is needed because you can have multiple wallets inside this awesome application!',
+              leftIcon: { name: 'wallet' }
+            },
+            {
+              title: 'Icon',
+              subtitle: 'Choose an icon that fits the purpose of this wallet perfectly',
+              leftIcon: { name: 'color-palette' }
+            }
+          ]
+        },
+        {
+          title: 'Credentials',
+          data: [
+            {
+              title: 'Paper key',
+              subtitle: 'The paper key enables you to restore this wallet. If you lose it you can never recover it and you\'ll loose access to your XVG. So make absolutely sure to keep it very safe.',
+              leftIcon: { name: 'key' }
+            },
+            {
+              title: 'Export your wallet',
+              subtitle: 'Creating an extra backup of your wallet credentials can be a smart thing to do. Either store it safely on a removable drive or another secure place.',
+              leftIcon: { name: 'download' }
+            }
+          ]
+        },
+        {
+          title: 'Danger Zone',
+          data: [
+            {
+              title: 'Delete',
+              subtitle: 'This will delete your wallet from this application. Your wallet can still be restored by using your paper key and pass phrase. Please make absolutely sure you\'ve written down your paper key and know your pass phrase. Remember that your paper key + pass phrase is the only way to restore your current wallet.',
+              leftIcon: { name: 'trash' }
+            }
+          ]
+        }
+      ]
+    }
+  },
 
   props: {
     navigation: {
