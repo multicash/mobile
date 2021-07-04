0e1e36c5ba7551f5a6a6bdb75585ae29702c46ff
Swen van Zanten <info@hdsign.nl>
2021-07-04 10:38:05 +0200

    change license

--- src/mixins/ColorScheme.ts
+++ src/mixins/ColorScheme.ts
@@ -1,7 +1,39 @@
-import Vue from 'vue-native-core'
-import { Appearance } from 'react-native'
+/*
+  MultiCash Mobile
+  Copyright (C) 2021  Swen van Zanten
 
-const store = new Vue({
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import Vue from 'vue-native-core'
+import { Appearance, AppState, AppStateStatus } from 'react-native'
+
+interface ColorStore extends Vue {
+  colorScheme?: string,
+  setColorScheme?: (colorScheme: string) => void
+}
+
+interface ColorMixin {
+  onAppStateChange?: () => void
+  handleAppearanceChange?: () => void
+  created: () => void
+  destroyed: () => void
+  computed: { isDarkScheme (): boolean; isLightScheme (): boolean; colorScheme (): string }
+  methods: { handleAppearanceChange (preferences: any): void; onAppStateChange (state: AppStateStatus): void }
+}
+
+const store: ColorStore = new Vue({
   data () {
     return {
       colorScheme: Appearance.getColorScheme()
@@ -15,39 +47,40 @@ const store = new Vue({
   }
 })
 
-Vue.mixin({
+const mixin: ColorMixin = {
   created () {
-    // @ts-ignore
-    Appearance.addChangeListener(this.handleAppearanceChange)
+    AppState.addEventListener('change', this.onAppStateChange!)
+    Appearance.addChangeListener(this.handleAppearanceChange!)
   },
 
   destroyed () {
-    // @ts-ignore
-    Appearance.removeChangeListener(this.handleAppearanceChange)
+    AppState.removeEventListener('change', this.onAppStateChange!)
+    Appearance.removeChangeListener(this.handleAppearanceChange!)
   },
 
   computed: {
     colorScheme () {
-      // @ts-ignore
-      return store.colorScheme
+      return store.colorScheme!
     },
 
     isDarkScheme () {
-      // @ts-ignore
       return store.colorScheme === 'dark'
     },
 
     isLightScheme () {
-      // @ts-ignore
       return store.colorScheme === 'light'
     }
   },
 
   methods: {
-    // @ts-ignore
-    handleAppearanceChange (preferences) {
-      // @ts-ignore
-      store.setColorScheme(preferences.colorScheme)
+    handleAppearanceChange (preferences: any): void {
+      store.setColorScheme!(preferences.colorScheme)
+    },
+
+    onAppStateChange () {
+      store.setColorScheme!(Appearance.getColorScheme() || 'light')
     }
   }
-})
+}
+
+Vue.mixin(mixin)
--- src/views/settings/AboutView.vue
+++ src/views/settings/AboutView.vue
@@ -1,18 +1,43 @@
+<!--MultiCash Mobile-->
+<!--Copyright (C) 2021  Swen van Zanten-->
+
+<!--This program is free software: you can redistribute it and/or modify-->
+<!--it under the terms of the GNU General Public License as published by-->
+<!--the Free Software Foundation, either version 3 of the License, or-->
+<!--any later version.-->
+
+<!--This program is distributed in the hope that it will be useful,-->
+<!--but WITHOUT ANY WARRANTY; without even the implied warranty of-->
+<!--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the-->
+<!--GNU General Public License for more details.-->
+
+<!--You should have received a copy of the GNU General Public License-->
+<!--along with this program.  If not, see <https://www.gnu.org/licenses/>.-->
+
 <template>
-  <view :style="{ flex: 1 }">
-    <modal-navigation
+  <modal-view
+    title="About MultiCash"
+    has-back-button
+    @on-dismiss="navigation.goBack()"
+  >
+    <header-view
       title="About MultiCash"
-      has-back-button
-      @on-dismiss="navigation.goBack()"
+      subtitle="Bunch of Moon boys!"
+      :image-background="require('@/assets/coins/MCX.png')"
     />
-    <view-background>
-      <header-view
-        title="About MultiCash"
-        subtitle="Bunch of Moon boys!"
-        :image-background="require('@/assets/coins/MCX.png')"
-      />
-    </view-background>
-  </view>
+    <text>
+      Are you sick and tired of all the convoluted solutions that are available on the open market today? We were too.
+    </text>
+    <text>
+      Our goal is to remove all the excess and give you what you need to succeed. By using MultiCash you are removing the middleman while regaining control over your future. MultiCash was created with you, the customer – the user, in mind. We are proud to provide multiple ways for you to send and receive digital currency on any platform of your choice.
+    </text>
+    <text>
+      With MultiCash there is no waiting, there is no “roadmap”, everything you need to get started is available now. While we do have development goals on our minds, we do not want to make false promises we cannot keep.
+    </text>
+    <text>
+      We take pride in delivering digital currency products that work.
+    </text>
+  </modal-view>
 </template>
 
 <script>
